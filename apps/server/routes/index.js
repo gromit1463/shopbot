f878170f1f0df3317bbafdbfe86d279dbfc4c80d
Stephen Hui <stephen@gingerfire.com>
2025-10-14 14:41:43 -0600

    Change root endpoint to html

--- apps/server/routes/index.js
+++ apps/server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.json({ hello: 'world' })
+	res.type('html').send('hello world')
 });
 
 module.exports = router;
